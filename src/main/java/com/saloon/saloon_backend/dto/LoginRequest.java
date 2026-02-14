792cab1a613f30afd69a6d96789cf89f9e0d020d
Arosha Udaraka <aroshaudaraka99@gmail.com>
2026-02-14 18:09:19 +0530

    modified dto login requst

--- src/main/java/com/saloon/saloon_backend/dto/LoginRequest.java
+++ src/main/java/com/saloon/saloon_backend/dto/LoginRequest.java
@@ -1,6 +1,23 @@
 package com.saloon.saloon_backend.dto;
 
 public class LoginRequest {
-    public String email;
-    public String password;
+    private String email;
+    private String password;
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
