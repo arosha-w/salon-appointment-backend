0f82706182f03387bdec1d4c5f8df623bbfe5e15
Arosha Udaraka <aroshaudaraka99@gmail.com>
2026-02-22 22:28:17 +0530

    add servicecreate dto

---
+++ src/main/java/com/saloon/saloon_backend/dto/ServiceCreateRequest.java
@@ -0,0 +1,14 @@
+package com.saloon.saloon_backend.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class ServiceCreateRequest {
+    private String name;
+    private String description;
+    private Integer durationMin;
+    private BigDecimal price;
+    private String category;
+    private Boolean isActive;
+}
